55fa69adaa202b60711356309c80d1e486363f4b
DmitryFursov <dmitryfursov177@gmail.com>
2020-05-25 14:43:56 +0300

    components update
    Post-Page
    Post
    Comment
    smallUser

--- src/app/post-page/post-page.component.ts
+++ src/app/post-page/post-page.component.ts
@@ -1,28 +1,41 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Post, Commentary } from '../shared/interfaces';
-import { Observable } from 'rxjs';
+import { Component, OnInit, Input, OnDestroy, AfterViewInit } from '@angular/core';
+import { Post, Commentary, User } from '../shared/interfaces';
+import { Observable, Subject, BehaviorSubject } from 'rxjs';
 import { ActivatedRoute, Params } from '@angular/router';
 import { PostsService } from '../user/shared/services/posts.service';
-import { switchMap, map, mergeMap, tap } from 'rxjs/operators';
+import { switchMap, map, mergeMap, tap, takeUntil } from 'rxjs/operators';
 import { AuthService } from '../user/shared/services/auth.service';
+import { ViewportScroller } from '@angular/common';
+import { UsersService } from '../user/shared/services/users.service';
 
 @Component({
   selector: 'app-post-page',
   templateUrl: './post-page.component.html',
   styleUrls: ['./post-page.component.scss']
 })
-export class PostPageComponent implements OnInit {
+export class PostPageComponent implements OnInit, OnDestroy, AfterViewInit {
 
+  author$: Observable<User>
   post$: Observable<Post>
   commentList$: Observable<Commentary[]> | null
+  private destroy$$ = new Subject();
+  private fragment$$ = new BehaviorSubject<string | null>(null);
+  private fragment$ = this.fragment$$.asObservable();
 
   commentText: string = '';
 
   constructor(
     private route: ActivatedRoute,
     private postsService: PostsService,
-    private auth: AuthService
-  ) { }
+    private auth: AuthService,
+    private vps: ViewportScroller,
+    private usersService: UsersService
+  ) {
+    this.route.fragment.pipe(takeUntil(this.destroy$$)).subscribe(fragment => {
+      this.fragment$$.next(fragment);
+    });
+
+  }
 
   ngOnInit() {
     this.post$ = this.route.params
@@ -30,6 +43,11 @@ export class PostPageComponent implements OnInit {
         return this.postsService.getPostById(params['id'])
       }))
     this.commentList$ = this.post$.pipe(map(post => { return post.commentList }))
+    this.author$ = this.post$
+      .pipe(
+        map(post => { return post.author }),
+        mergeMap(authorId => { return this.usersService.getById(authorId) })
+      );
   }
 
   submit() {
@@ -40,8 +58,6 @@ export class PostPageComponent implements OnInit {
     }
 
     this.commentText = ''
-
-
     this.commentList$ = this.route.params
       .pipe(
         switchMap((params: Params) => {
@@ -61,4 +77,20 @@ export class PostPageComponent implements OnInit {
         )
       )
   }
+
+  public ngAfterViewInit(): void {
+    this.fragment$.pipe(takeUntil(this.destroy$$)).subscribe(fragment => {
+      if (!!fragment) {
+        //        document.querySelector('#' + fragment).scrollIntoView();
+        this.vps.scrollToAnchor(fragment)
+      }
+    });
+  }
+
+  public ngOnDestroy(): void {
+    this.destroy$$.next();
+    this.destroy$$.complete();
+  }
 }
+
+
